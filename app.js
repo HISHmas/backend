caa31ad2f079f1affac301b1370c7befc0f70c39
subeend <happysong98@gmail.com>
2025-11-20 23:57:03 +0900

    chore: 포트번호 변경 및 cors 설정

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 // app.js
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-
 app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
 
-// 라우트 test
+// 기본 라우트
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+// ping 유지
 app.get("/ping", (req, res) => {
     res.send("pong");
 });
-
-
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// index.js
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
