65fdaf9b0c160d472a617ab31a89cb73f5f70f98
subeend <happysong98@gmail.com>
2025-11-22 18:46:42 +0900

    feat: swagger 문서 추가

--- src/server.js
+++ src/server.js
@@ -5,4 +5,5 @@ const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
+    console.log(`📘 Swagger Docs: http://localhost:${PORT}/api-docs`);
 });
